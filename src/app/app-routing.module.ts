e337f76defa0281f45eeb343f3c1a5c1a80f3d3a
gabrielMaia-1 <gab.maia@outlook.com>
2022-02-10 15:42:52 -0400

    adiionado redirect rota not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { LoginComponent } from './shared/components/login/login.component';
 const routes: Routes = [
   {path: 'login', component: LoginComponent, canActivate: [AuthGuardService]},
   {path: 'home', component: HomeComponent, loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule), canActivate: [AuthGuardService]},
-  {path: '',pathMatch:'full', redirectTo: 'home'}
+  {path: '**',pathMatch:'full', redirectTo: 'home'}
 ];
 
 @NgModule({
